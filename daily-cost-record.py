d6a8b634a8bb703c0b3c8c05aca70a9122082da6
Hieu Thuan Lam <hieulam1312@gmail.com>
2022-04-20 14:29:15 +0700

    Update daily-cost-record.py

--- daily-cost-record.py
+++ daily-cost-record.py
@@ -1,11 +1,4 @@
-import datetime as dt
-import gspread
+
 import streamlit as st
-import pandas as pd
-import numpy as np
-from google.oauth2 import service_account
-from datetime import datetime
-import pytz
-tz = pytz.timezone('asia/ho_chi_minh')
 
 st.title('hello world')
